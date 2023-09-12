18d0969f0f090e5ec36d87ff791ab557b43261b4
Gabriel <gabrielguima2001@gmail.com>
2023-09-12 19:32:22 -0300

    Refatorei o componente nota com encapsulamento

--- bloco_notas/src/App.js
+++ bloco_notas/src/App.js
@@ -2,20 +2,32 @@ import './App.css';
 
 function App() { 
   const nota = "Minha primeira nota";
-  const titulo= "Titulo da anotação";
+  const titulo= "Minha primeira anotação";
   const data = "12/09/2023";
+  const titulo2 = "Minha segunda anotação"
+  const data2 = "13/09/2023"
+  const nota2= "Minhas anotações melhoradas"
   return (
     <div className="App">
       <h1>Bloco de notas </h1>
-      <div>
-          <div>{data}</div>
-
-          <div>{titulo}</div>
-
-          <div>{nota}</div>
-      </div>
+      <Nota data={data} titulo={titulo} nota={nota}> </Nota>
+      
+      <Nota data={data2} titulo={titulo2} nota={nota2}> </Nota>
+      
     </div>
   );
 }
 
+function Nota({data,titulo,nota}) {           
+
+  return (
+    <div> 
+      <div>{data}</div>
+
+      <div>{titulo}</div>
+
+      <div>{nota}</div>
+    </div>
+  )
+}
 export default App;
